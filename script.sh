64eee35c4d124dfc73782615cf51ab30ff0d8db4
anthony.verkindere <anthony.verkindere@epita.fr>
2023-06-16 09:25:56 +0200

    feat(script): script for q3

--- script.sh
+++ script.sh
@@ -3,5 +3,10 @@ if [ $1 = "q2" ]; then
     python3 testsuite/q2/starTree.py
     python3 testsuite/q2/simpleTree.py
     python3 testsuite/q2/complexTree.py
+elif [ $1 = "q3" ]; then
+    python3 testsuite/q3/binaryTree.py
+    python3 testsuite/q3/starTree.py
+    python3 testsuite/q3/simpleTree.py
+    python3 testsuite/q3/complexTree.py
 fi
 
