a37a82b924511194382e8f2f7abeebb8a8df98b6
anthony.verkindere <anthony.verkindere@epita.fr>
2023-06-16 09:51:56 +0200

    fix(q3): update algo and add test

--- code/q3.py
+++ code/q3.py
@@ -1,12 +1,22 @@
 visited = set()
+visited_edges = set()
 overseers = set()
 def tree_surveillance(tree, node, overseer):
     if overseer:
         overseers.add(node)
+        for e in tree.edges(node):
+            visited_edges.add(e)
+
     for n in tree.neighbors(node):
         if n in visited:
             continue
         visited.add(n)
         tree_surveillance(tree, n, not overseer)
+
+    for e in tree.edges():
+        if e not in visited_edges:
+            overseers.add(e[0])
+            for e in tree.edges(e[0]):
+                visited_edges.add(e)
     return overseers
-        
+
