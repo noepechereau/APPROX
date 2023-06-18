9bed976411bd75394136d5a2a4a98adb5698a432
Virgile Hermant <virgile1.hermant@epita.fr>
2023-06-18 20:35:47 +0200

    feat: comparaison

---
+++ testsuite/comparaison/completeTest.py
@@ -0,0 +1,61 @@
+import time
+
+import networkx as nx
+
+import importlib.machinery
+import importlib.util
+from pathlib import Path
+
+import os
+
+path = './assets'
+if not os.path.exists(path):
+    os.mkdir(path)
+
+script_dir = Path(__file__).parent
+mymodule_path = str(script_dir.joinpath('utils.py'))
+
+loader = importlib.machinery.SourceFileLoader('utils', mymodule_path)
+spec = importlib.util.spec_from_loader('utils', loader)
+utils = importlib.util.module_from_spec(spec)
+loader.exec_module(utils)
+
+
+def data():
+    # yield nx.complete_graph(15), "complet_15_noeuds"
+    # yield nx.circular_ladder_graph(9), "circular_ladder_18_noeuds"
+    # yield nx.circular_ladder_graph(8), "circular_ladder_16_noeuds"
+    # yield nx.circulant_graph(8, [1]), "circulant_8_noeuds",
+    # yield nx.circulant_graph(8, [1, 2]), "circulant_8_noeuds_2"
+    # yield nx.circulant_graph(16, [1]), "circulant_16_noeuds"
+    # yield nx.circulant_graph(18, [1, 3]), "circulant_18_noeuds"
+    # yield nx.dorogovtsev_goltsev_mendes_graph(3), "dorogovtsev_3"
+    # yield nx.dorogovtsev_goltsev_mendes_graph(2), "dorogovtsev_2"
+    # yield nx.full_rary_tree(6, 13), "full_6ary_tree_13_noeuds"
+    # yield nx.full_rary_tree(3, 16), "full_3ary_tree_16_noeuds"
+    # yield nx.full_rary_tree(2, 18), "full_2ary_tree_18_noeuds"
+    # yield nx.ladder_graph(5), "ladder_10_noeuds"
+    # yield nx.ladder_graph(9), "ladder_18_noeuds"
+    # yield nx.lollipop_graph(5,5), "lollipop_10_noeuds"
+    # yield nx.lollipop_graph(10, 2), "lollipop_12_noeuds"
+    # yield nx.path_graph(15), "path_15_noeuds"
+    # yield nx.star_graph(14), "star_14_noeuds"
+    # yield nx.star_graph(15), "star_15_noeuds"
+    # yield nx.star_graph(16), "star_16_noeuds"
+    # yield nx.turan_graph(15,7), "turan_15_noeuds_7_r"
+    # yield nx.turan_graph(15, 3), "turan_15_noeuds_3_r"
+    # yield nx.turan_graph(15, 2), "turan_15_noeuds_2_r"
+    # yield nx.wheel_graph(15), "wheel_15_noeuds"
+    yield utils.gnp_random_connected_graph(15, 0.3), "random_15_noeuds_30"
+    yield utils.gnp_random_connected_graph(16, 0.3), "random_16_noeuds_30"
+    yield utils.gnp_random_connected_graph(15, 0.2), "random_15_noeuds_20"
+    yield utils.gnp_random_connected_graph(16, 0.2), "random_16_noeuds_20"
+    yield utils.gnp_random_connected_graph(15, 0.02), "random_15_noeuds_002"
+    yield utils.gnp_random_connected_graph(16, 0.02), "random_16_noeuds_002"
+
+
+start = time.time()
+for graph, name in data():
+    utils.solution(graph, name, path)
+end = time.time() - start
+print("Temps écoulé : {}s".format(round(end, 2)))
---
+++ testsuite/comparaison/utils.py
@@ -0,0 +1,66 @@
+import importlib.machinery
+import importlib.util
+import random
+import time
+from itertools import combinations, groupby
+from pathlib import Path
+import networkx as nx
+import matplotlib.pyplot as plt
+
+script_dir = Path(__file__).parent
+mymodule_path = str(script_dir.joinpath('..', '..', 'code', 'q5.py'))
+
+loader = importlib.machinery.SourceFileLoader('q5', mymodule_path)
+spec = importlib.util.spec_from_loader('q5', loader)
+q5 = importlib.util.module_from_spec(spec)
+loader.exec_module(q5)
+
+mymodule_path = str(script_dir.joinpath('..', '..', 'code', 'q3.py'))
+
+loader = importlib.machinery.SourceFileLoader('q3', mymodule_path)
+spec = importlib.util.spec_from_loader('q3', loader)
+q3 = importlib.util.module_from_spec(spec)
+loader.exec_module(q3)
+
+
+def solution(graph, name, path):
+
+    # OPTI
+    start_opti = time.time()
+    noeuds_surveilles = q5.general_case(graph)
+    elapsed_opti = time.time() - start_opti
+    node_colors = ['red' if noeud in noeuds_surveilles else 'lightblue' for noeud in graph.nodes()]
+    nx.draw(graph, with_labels=True, node_color=node_colors, node_size=500, font_size=10)
+    plt.savefig(path + "/" + name + "_exact")
+    plt.close()
+
+    # APPROX
+    start_approx = time.time()
+    noeuds_surveilles = q3.tree_surveillance(graph, 0, True)
+    elapsed_approx = time.time() - start_approx
+    node_colors = ['red' if noeud in noeuds_surveilles else 'lightblue' for noeud in graph.nodes()]
+    nx.draw(graph, with_labels=True, node_color=node_colors, node_size=500, font_size=10)
+    plt.savefig(path + "/" + name + "_approx")
+    plt.close()
+
+    print("{} :".format(name.replace("_", " ")))
+    print("  Temps algo exact: {}s".format(round(elapsed_opti, 2)))
+    print("  Temps algo approx: {}s".format(round(elapsed_approx, 2)))
+
+
+def gnp_random_connected_graph(n, p):
+    edges = combinations(range(n), 2)
+    G = nx.Graph()
+    G.add_nodes_from(range(n))
+    if p <= 0:
+        return G
+    if p >= 1:
+        return nx.complete_graph(n, create_using=G)
+    for _, node_edges in groupby(edges, key=lambda x: x[0]):
+        node_edges = list(node_edges)
+        random_edge = random.choice(node_edges)
+        G.add_edge(*random_edge)
+        for e in node_edges:
+            if random.random() < p:
+                G.add_edge(*e)
+    return G
