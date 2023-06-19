1151cab02ee6aadcd1efdc546662286e302a1731
noepechereau <noe.pechereau@epita.fr>
2023-06-19 13:45:09 +0100

    feat(clear): clear README and testsuite

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # APPROX
 
-to run the testsuite please run:
+to run the testsuite please run "./script.sh" + your option:
 
-./script.sh q2
+./script.sh --tree
+
+to run the testsuite with the algorithm of VERTEX Cover with a tree.
+
+./script.sh --approx
+
+to run the testsuite with the algorithm of the APPROX algorithm in a general case.
+
+./script.sh compare--opti-tree
+
+to compare the q2 algo with the q5 algo
+
+./script.sh --opti
+
+to run the testsuite with the optimal algorithm in a general case.
+
+./script.sh --compare-approx-opti
+
+to compare the optimal algo and the approx algo.
--- script.sh
+++ script.sh
@@ -1,25 +1,24 @@
-if [ $1 = "q2" ]; then
+if [ $1 = "--tree" ]; then
     python3 testsuite/q2/binaryTree.py
     python3 testsuite/q2/starTree.py
     python3 testsuite/q2/simpleTree.py
     python3 testsuite/q2/complexTree.py
-elif [ $1 = "q3" ]; then
+elif [ $1 = "--approx" ]; then
     python3 testsuite/q3/binaryTree.py
     python3 testsuite/q3/starTree.py
     python3 testsuite/q3/simpleTree.py
     python3 testsuite/q3/simpleGraph.py
     python3 testsuite/q3/complexTree.py
     python3 testsuite/q3/complexGraph.py
-elif [ $1 = "q4" ]; then
+elif [ $1 = "--compare-opti-tree" ]; then
     python3 testsuite/comparaison/completeTree.py
-elif [ $1 = "q5" ]; then
+elif [ $1 = "--opti" ]; then
     python3 testsuite/q5/binaryTree.py
     python3 testsuite/q5/starTree.py
     python3 testsuite/q5/simpleTree.py
     python3 testsuite/q5/simpleGraph.py
     python3 testsuite/q5/complexTree.py
     python3 testsuite/q5/complexGraph.py
-else
+elif [ $1 = "--compare-approx-opti"]; then
     python3 testsuite/comparaison/completeTest.py
 fi
-
