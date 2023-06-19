9379a0fbeaf2d242dbb507985b9dfc62de7b9678
noepechereau <noe.pechereau@epita.fr>
2023-06-19 13:57:58 +0100

    fix(anthony est un dog waf waf)

--- code/q2.py
+++ code/q2.py
@@ -1,11 +1,13 @@
 surveilles = set()
 
 def tree_surveillance(tree, node, parent):
+    surveilles = set()
+    tree_surveillance(tree, node, parent)
+
+def tree_surveillance_rec(tree, node, parent):
     for n in tree.neighbors(node):
         if n != parent:
             tree_surveillance(tree, n, node)
             if n not in surveilles:
                 surveilles.add(node)
     return surveilles
-
-
