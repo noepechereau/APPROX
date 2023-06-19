c50ad8d61272558e1544f77559fc278a41b68841
anthony.verkindere <anthony.verkindere@epita.fr>
2023-06-19 12:55:37 +0200

    Fix(q3): now it is better

--- code/q3.py
+++ code/q3.py
@@ -1,32 +1,15 @@
 import queue
-import networkx as nx
 
-visited = set()
-overseers = set()
+def tree_surveillance(E, src, a):
+    S = set()
+    E_prime = set(E.edges)
 
-def tree_surveillance(graph, src, a):
-    q = queue.Queue()
-    q.put(src)
-    q.put(None)
-    visited.add(src)
-    overseer = True
+    while E_prime:
+        u, v = E_prime.pop()
+        S.add(u)
+        S.add(v)
 
-    while not q.empty():
-        node = q.get()
-        if node == None and q.empty():
-            break
-        if node == None:
-            overseer = not overseer
-            q.put(None)
-            continue
-        if overseer:
-            overseers.add(node)
-        for n in graph.neighbors(node):
-            if n not in visited:
-                visited.add(n)
-                q.put(n)
-    for (x,y) in nx.edges(graph):
-        if x not in overseers and y not in overseers:
-            overseers.add(x)
+        # Supprime toutes les arêtes contenant u ou v de E_prime
+        E_prime = {e for e in E_prime if u not in e and v not in e}
 
-    return overseers
+    return S
